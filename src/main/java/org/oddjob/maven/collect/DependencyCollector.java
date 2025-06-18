02c63985724d9635f1c0d2a871fa2b7f4f15c5b1
Rob Gordon <rob@rgordon.co.uk>
2025-06-18 07:28:47 +0100

    Remove failing tests.

--- src/main/java/org/oddjob/maven/collect/DependencyCollector.java
+++ src/main/java/org/oddjob/maven/collect/DependencyCollector.java
@@ -1,14 +1,15 @@
 package org.oddjob.maven.collect;
 
 import org.eclipse.aether.collection.CollectResult;
-import org.eclipse.aether.repository.RemoteRepository;
 import org.oddjob.maven.types.DependencyContainer;
 
-import java.util.List;
-
+/**
+ * Abstraction for Collecting Dependencies.
+ * <p>
+ * In Ant this is in {@code AntRepoSys}
+ *
+ */
 public interface DependencyCollector {
 
     CollectResult collectDependencies(DependencyContainer dependencies) ;
-
-    List<RemoteRepository> getRemoteRepositories();
 }
--- src/test/java/org/oddjob/maven/collect/DependencyCollectorTest.java
+++ src/test/java/org/oddjob/maven/collect/DependencyCollectorTest.java
@@ -3,7 +3,6 @@ package org.oddjob.maven.collect;
 import org.apache.maven.settings.building.SettingsBuildingException;
 import org.eclipse.aether.collection.CollectResult;
 import org.eclipse.aether.graph.DependencyNode;
-import org.eclipse.aether.repository.RemoteRepository;
 import org.junit.Test;
 import org.oddjob.arooa.ArooaSession;
 import org.oddjob.arooa.standard.StandardArooaSession;
@@ -12,8 +11,6 @@ import org.oddjob.maven.props.RepoSessionProperties;
 import org.oddjob.maven.session.ResolverSession;
 import org.oddjob.maven.session.ResolverSessionBuilder;
 import org.oddjob.maven.types.Dependency;
-
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -64,51 +61,4 @@ public class DependencyCollectorTest {
         assertThat(child12.getArtifact().toString(), is("commons-collections:commons-collections:jar:3.2.2"));
     }
 
-    @Test
-    public void testGetRemoteRepositories() throws SettingsBuildingException {
-
-        ArooaSession arooaSession = new StandardArooaSession();
-
-        RepoSessionProperties sessionProperties = ArooaRepoProperties.from(
-                arooaSession.getPropertyManager());
-
-        ResolverSession resolverSession = ResolverSessionBuilder.from(sessionProperties)
-                .build();
-
-        DependencyCollector dependencyCollector = DependencyCollectorBuilder.from(resolverSession)
-                .build();
-
-        List<RemoteRepository> repoList = dependencyCollector.getRemoteRepositories();
-
-        assertThat(repoList.size(), is(1));
-
-        RemoteRepository defaultRepo = repoList.get(0);
-
-        assertThat(defaultRepo.getId(), is("central"));
-    }
-
-    @Test
-    public void testMergedRemoteRepositories() throws SettingsBuildingException {
-
-        ArooaSession arooaSession = new StandardArooaSession();
-
-        RepoSessionProperties sessionProperties = ArooaRepoProperties.from(
-                arooaSession.getPropertyManager());
-
-        ResolverSession resolverSession = ResolverSessionBuilder.from(sessionProperties)
-                .build();
-
-        DependencyCollector dependencyCollector = DependencyCollectorBuilder.from(resolverSession)
-                .withNoSettingsRepos(true)
-                .build();
-
-        List<org.eclipse.aether.repository.RemoteRepository> repoList =
-                dependencyCollector.getRemoteRepositories();
-
-        assertThat(repoList.size(), is(1));
-
-        org.eclipse.aether.repository.RemoteRepository repo = repoList.get(0);
-
-        assertThat(repo.getUrl(), is("https://repo1.maven.org/maven2/"));
-    }
 }
